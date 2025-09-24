d1133c4fd40a4c719fd2850c623ac4864fca6ff1
Alexander Kurakin <kuraga333@mail.ru>
2025-09-24 13:21:39 +0300

    docs: improvements

--- lazyscribe/release.py
+++ lazyscribe/release.py
@@ -27,17 +27,17 @@ LOG = logging.getLogger(__name__)
 
 @define
 class Release:
-    """Create a release associated with a Repository instance.
+    """Create a release associated with a repository.
 
     Parameters
     ----------
     tag : str
-        A string descriptor for the release. Commonly coincides with semantic
+        A string identifier of the release. Commonly coincides with semantic
         or calendar versioning.
     artifacts : list[tuple[str, int]]
-        A list of the latest available artifacts and versions in the source repository.
+        A list of the latest available artifacts' names and versions in the source repository.
     created_at : datetime.datetime, optional (default ``lazyscribe._utils.utcnow()``)
-        The creation timestamp for the release (in UTC).
+        When the release was created (in UTC).
     """
 
     tag: str = field()
@@ -50,7 +50,12 @@ class Release:
         Returns
         -------
         dict
-            A dictionary with the release information.
+            Represent the release, with the following keys:
+            
+            * ``tag`` (``str``): a string identifier of the release.
+            * ``artifacts`` (``list[lazyscribe.artifacts.base.Artifact]``):
+                a list of the latest available artifacts' names and versions in the source repository.
+            * ``created_at`` (``datetime.datetime``): the creation timestamp for the release.
         """
         return {
             "tag": self.tag,
@@ -60,7 +65,7 @@ class Release:
 
     @classmethod
     def from_dict(cls, info: dict[str, Any]) -> Release:
-        """Convert a serialized representation of the release back to a python object.
+        """Convert a serialized representation back to a release instance.
 
         Parameters
         ----------
@@ -70,7 +75,7 @@ class Release:
         Returns
         -------
         lazyscribe.release.Release
-            The new release object.
+            The new release.
         """
         return cls(
             tag=info["tag"],
@@ -90,7 +95,7 @@ def create_release(repository: Repository, tag: str) -> Release:
     repository : lazyscribe.repository.Repository
         The source repository.
     tag : str
-        A string descriptor of the tag. Commonly coincides with semantic or calendar
+        A string identifier of the release. Commonly coincides with semantic or calendar
         versioning.
 
     Returns
@@ -140,7 +145,7 @@ def find_release(
     Returns
     -------
     lazyscribe.release.Release
-        The release object.
+        The release.
 
     Raises
     ------
@@ -213,7 +218,7 @@ def dump(obj: list[Release], fp: IOBase, **kwargs: Any) -> None:
     Parameters
     ----------
     obj : list[lazyscribe.release.Release]
-        The list of release objects.
+        The list of releases.
     fp : io.IOBase
         A buffer we can write to.
     **kwargs
@@ -238,7 +243,7 @@ def dumps(obj: list[Release], **kwargs: Any) -> str:
     Parameters
     ----------
     obj : list[lazyscribe.release.Release]
-        The list of release objects.
+        The list of releases.
     **kwargs
         Keyword arguments for ``json.dumps``.
 
